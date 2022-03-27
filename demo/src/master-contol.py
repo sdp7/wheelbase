348eb8e61a2fb1d188e11c799f9e674c45bfd162
arakban <arka.purkayastha10@gmail.com>
2022-03-27 13:55:16 +0100

    skeleton for master_control

---
+++ demo/src/master-contol.py
@@ -0,0 +1,12 @@
+from  navArena import *
+from mannualControll import *
+
+
+if __name__ == '__main__':
+    navAI = NavClient() 
+    mCI = mannualController()
+
+    #subscriber to a class listening to if manual control is one:
+
+    if subscriber = "manual_on":
+        #run appropiate publishing func
